2a2aa1dc5508decf7802bb0d1d9d341a0645110c
sydneyjenkin <113078386+sydneyjenkin@users.noreply.github.com>
2022-12-02 09:39:16 -0500

    added preliminary function to call to weather api
    decides genre and feeds back to call to movie api

--- prototype/app.py
+++ prototype/app.py
@@ -2,18 +2,59 @@ import requests
 from getpass import getuser
 import flask
 from flask import Flask, Response, request, render_template, redirect, url_for
+from numpy import randint
 
 
 import os, base64
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 
-@app.route('/pic', methods =["POST"])
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('weather', methods = ["GET"])
+def getMovieForWeather():
+    result = getWeather()
+    #CALL TO IMDB API BASED ON GENRE RETURN
+
+def getWeather():
+    genre = ""
+    genres = ['comedy', 'drama', 'science fiction', 'horror', 'western'\
+        'thriller', 'action', 'fantasy', 'mystery', 'romance', 'holiday']
+
+    if flask.request.method == 'GET':
+        weatherUrl = "" #WEATHER API HERE
+
+        weatherHeaders = {} #API KEYS HERE
+        
+        weatherResponse = requests.request("GET", weatherUrl, headers=weatherHeaders)
+
+        weatherResponseJ = weatherResponse.json()
+
+        description = "" #FIND WEATHER DESCRIPTION
+        highTemp = "" #FIND HIGH TEMP
+        lowTemp = "" #FIND LOW TEMP
+
+        #DECIDE GENRE ASSOCIATIONS
+        if description == "Sunny":
+            genre = genres[randint(0, 7, 9)]
+        elif description == "Rain":
+            if highTemp > 60:
+                genre = genres[randint(1, 5, 8)]
+            else:
+                genre = genres[randint()] 
+        elif description == "Cloudy":
+            genre = ""
+        elif description == "":
+
+
+@app.route('/pic', methods =["GET"])
 def getposter():
 
     if flask.request.method == 'GET':	
         url = "https://imdb8.p.rapidapi.com/auto-complete"
-        movie = request.form['movieTitle']
+        movie = input("Enter movie:")
         
     #movie = "the shawshank redemption"
 
@@ -33,7 +74,8 @@ def getposter():
 
         return responsej['d'][0]['i']['imageUrl']
 
-
+if __name__ == '__main__':
+    app.run()
 
 #movie = input("Enter movie:")
 #print("Username is: " + movie)
