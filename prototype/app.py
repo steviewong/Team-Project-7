dab4a33c27e1624d41a2a6d2625bda3a758847e1
sydneyjenkin <sjenkin@bu.edu>
2022-12-08 18:32:18 -0500

    added login and logout functions, started adding watchlist setup

--- prototype/app.py
+++ prototype/app.py
@@ -1,9 +1,9 @@
 #imports
-import requests, flask, flask_sqlalchemy, flask_login, numpy, dotenv, logging, oauthlib
+import requests, flask, flask_sqlalchemy, flask_login, numpy, dotenv, logging, oauthlib, flask_bcrypt
 from getpass import getuser
 from flask import Flask, Response, request, render_template, redirect, url_for, session, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, current_user, login_required, login_user, logout_user, UserMixin
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user, UserMixin, LoginForm, is_safe_url
 from flask_wtf import FlaskForm
 from authlib.integrations.flask_client import OAuth
 from dotenv import load_dotenv
@@ -32,24 +32,78 @@ db = SQLAlchemy(app)
 #init login manager
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = 'login.html'
 
 #set up User class which will represent each dataset in the database
 class User(UserMixin, db.Model):
-    __tablename
+    __tablename__ = 'Accounts'
 
     firstName = db.Column(db.String(30), nullable=True)
     lastName = db.Column(db.String(30), nullable=True)
     email = db.Column(db.String(50), primary_key=True)
     password = db.Column(db.String(20), nullable=False)
-    timeCreated = db.Column(db.DateTime(timezone=True), server_default=func.now())
     moviesToWatch = db.Column(db.Text)
 
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f'<Logged in under {self.email}>'
+    
+    def get_id(self):
+        return self.email
+
+    def is_anonymous(self):
+        return False
+
+    def getMovies(self):
+        return self.moviesToWatch
+    
+    def addMovie(self, movieTitle):
+        self.moviesToWatch += movieTitle + '\n'
+
 
 @login_manager.user_loader
-def load_user(username):
-    return User.get(username)
+def load_user(user_id):
+    return User.get(user_id)
+
+#APP ROUTES
+@app.route('/')
+@app.route('/static/home')
+def main():
+    return render_template('home.html')
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.get(form.email.data)
+        if user:
+            if flask.bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user, remember=True)
+                return render_template('moviegen.html')
+            flask.flash('Incorrect password')
+    return render_template('login.html', form=form)
+
+@app.route('/logout', methods = ['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('app.home'))
+
+@app.route('/toWatch')
+@login_required
+def toWatch(user_id):
+    movies = User.getMovies()
+    return render_template('watchList.html', movies=movies)
+
+@app.route('/static/moviegen')
+@login_required
+def afterLogin():
+    return render_template('moviegen.html')
+
+@app.route('/editWatchlist')
+@login_required
+def editWatchlist():
+    movie = request.form.get('movieTitle')
+    current_user.addMovie(movie)
 
 #route to add new user to database
 @app.route('/createUser', methods=['GET', 'POST'])
@@ -57,20 +111,16 @@ def createUser():
     if flask.request.method == 'POST':
         username = flask.request.form['username']
         firstName = flask.request.form['firstName']
-        lastName = flask.request.form['lasstName']
+        lastName = flask.request.form['lastName']
         email = flask.request.form['email']
         password = flask.request.form['password']
         
         db.session.add(User)
         db.session.commit()
 
-        return flask.redirect(url_for('moviegen'))
+        return render_template('moviegen.html')
 
     return render_template('createUser.html') #code for lines 22, 87-101 adapted from https://www.digitalocean.com/community/tutorials/how-to-use-flask-sqlalchemy-to-interact-with-databases-in-a-flask-application
-
-@app.route('/')
-def main():
-    return render_template('home.html')
 
 #route for if user selects weather option to generate movie
 @app.route('/static/getMovieForWeather', methods = ['GET', 'POST'])
